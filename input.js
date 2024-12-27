7d58c38f54683993e9ea2a9a297b9b870a88fb40
Perumalla Chandrashekar <csperumalla11@gmail.com>
2024-12-27 23:28:24 +0530

    Update input.js

--- input.js
+++ input.js
@@ -1,1 +1,23 @@
+function validationNumber() {
+    const inputField = document.getElementById('inputText')
+    const messageEl = document.getElementById('message');
+    const number = parseInt(inputField.value)
 
+    if (isNaN(number)) {
+        messageEl.textContent = "Please enter a valid number";
+        messageEl.classList.add("red")
+        return
+    }
+
+    if (number < 0) {
+        messageEl.textContent = "Enter a positive value"
+        messageEl.classList.add("dark-red")
+        
+    }else if (number % 2 === 0) {
+        messageEl.textContent = `Next 3 even numbers: ${number +2}, ${number +4}, ${number +6}`
+        messageEl.classList.add("green")
+    }else{
+        messageEl.textContent = `Next 3 odd numbers: ${number +2}, ${number +4}, ${number +6}`
+        messageEl.classList.add("green")
+    }
+}
